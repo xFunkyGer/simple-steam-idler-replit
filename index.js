030f3fea4af726fd73cf948e1b9297df71631b14
xFunkyGer <156592255+xFunkyGer@users.noreply.github.com>
2024-02-10 23:27:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [1454400, 363970, 346900, 1353300, 232090, 1250, 1891700, 1397920, 735850, 2153770, 1482860];  // Enter here AppIDs of the needed games
+var games = [1454400, 363970, 346900, 1353300, 232090, 1250, 1891700, 1397920, 735850, 2153770, 960090, 1482860];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
